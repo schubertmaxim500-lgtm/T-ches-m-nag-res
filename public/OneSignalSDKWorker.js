6c8b35ee8aac694e3c4b893f772fca6cbf6405c5
schubertmaxim500-lgtm <schubertmaxim500@gmail.com>
2026-03-31 12:13:31 +1100

    Delete public/OneSignalSDKWorker.js

--- public/OneSignalSDKWorker.js
+++
@@ -1,3 +0,0 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-self.addEventListener('install', function(event) { self.skipWaiting(); });
-self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });
